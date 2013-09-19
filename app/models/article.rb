41a0a3aea117f2b6fe5a8df86392c344289ddd45
mbove <mbove@andrew.cmu.edu>
2013-09-18 22:23:59 -0400

    added article views and controller successfully

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
 	validates_presence_of :title, :content
 
+	belongs_to :category
+
 	scope :alphabetical, order('title')
   	scope :active, where('active = ?', true)
 end
